1640b7b1186dcc695394825428e3215ba79468ac
John Casey <jdcasey@commonjava.org>
2021-09-27 13:57:19 -0500

    add docs for the python script

--- measure.py
+++ measure.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+
+
 import time
 import board
 import busio
@@ -10,6 +12,7 @@ from ruamel.yaml import YAML
 import os
 import sys
 
+# These are configuration keys, to be found in your config.yaml file.
 WRITE_KEY = 'write_key'
 DATASET = 'dataset'
 SAMPLE_FREQUENCY = 'sample_frequency'
@@ -18,14 +21,20 @@ TVOC_BASELINE = 'tvoc_baseline'
 NODE_ID = 'node_id'
 QUIET_MODE = 'quiet'
 
+# Require the config-file to be present as a command-line arg. This works best with Systemd.
 if len(sys.argv) < 2:
     print(f"Usage: {sys.argv[0]} [CONFIG_YAML_PATH]")
     exit(1)
 
-CONFIG_PATH = os.path.join(sys.argv[1])
+# Path to the configuration file, which is referenced in a couple of places.
+CONFIG_PATH = sys.argv[1]
 
 
 def read_config():
+    """
+    Read the configuration YAML file into a dict.
+    :return: The dict containing the configuration
+    """
     if os.path.exists(CONFIG_PATH):
         with open(CONFIG_PATH) as f:
             return YAML().load(f)
@@ -34,6 +43,13 @@ def read_config():
 
 
 def read_particulates(sensor, event, quiet=False):
+    """
+    Read the PMSA003I air quality sensor to get particulate data.
+    :param sensor: The sensor object to read from
+    :param event: The Honeycomb event into which sensor reading should be added. Can be None
+    :param quiet: If True (and the Honeycomb event is available), sensor readings are suppressed from STDOUT
+    :return: None
+    """
     try:
         aqdata = sensor.read()
         # print(aqdata)
@@ -41,7 +57,9 @@ def read_particulates(sensor, event, quiet=False):
         print("Unable to read from sensor, retrying...")
         return
 
-    if not quiet:
+    if event is None or quiet is False:
+        # NOTE: This output format was copied from the Adafruit example for this sensor, at:
+        # https://github.com/adafruit/Adafruit_CircuitPython_PM25/blob/main/examples/pm25_simpletest.py#L60-L80
         print()
         print("Concentration Units (standard)")
         print("---------------------------------------")
@@ -70,19 +88,28 @@ def read_particulates(sensor, event, quiet=False):
 
 
 def read_volatiles(sensor, since_baseline, event, quiet=False):
+    """
+    Read the SGP30 MOX sensor to get CO₂ and VOC data
+    :param sensor: The SGP30 sensor to read
+    :param since_baseline: The last time we collected / printed candidate baseline settings (useful for calibrating)
+    :param event: The Honeycomb event, into which sensor readings should be reported
+    :param quiet: If True (and the Honeycomb event is available), sensor readings are suppressed from STDOUT
+    :return: The new since_baseline counter value
+    """
     since_baseline += 1
 
-    if not quiet:
+    if event is None or quiet is False:
         print(f"eCO2 = {sensor.eCO2} ppm \t TVOC = {sensor.TVOC} ppb")
 
     if event is not None:
         event.add_field("eCO2_ppm", sensor.eCO2)
         event.add_field("TVOC_ppb", sensor.TVOC)
 
+    # Print / report a new candidate baseline every 10th measurement. This helps us calibrate the sensor when needed.
     if since_baseline > 10:
         since_baseline = 0
 
-        if not quiet:
+        if event is None or quiet is False:
             print(f"Baseline values: eCO2 = 0x{sensor.baseline_eCO2:x}, TVOC = 0x{sensor.baseline_TVOC:x}")
 
         if event is not None:
@@ -93,6 +120,11 @@ def read_volatiles(sensor, since_baseline, event, quiet=False):
 
 
 def init_electronics(config):
+    """
+    Setup the sensors and the I²C bus. If we have a baseline configuration for the SGP30 sensor, set that here too.
+    :param config: The configuration dict as read from the YAML file
+    :return: The sensor objects, for use in the rest of the application
+    """
     reset_pin = None
 
     print("Starting I2C bus...")
@@ -112,46 +144,66 @@ def init_electronics(config):
 
     sgp30.iaq_init()
     if config.get(CO2_BASELINE) is not None and config.get(TVOC_BASELINE) is not None:
+        # This is the baseline that Adafruit mentions in their example code.
         # sgp30.set_iaq_baseline(0x8973, 0x8AAE)
+        # We'll set the calibration baseline from what we have in the config YAML
+        # (which was detected by running the sensor outside for 10 mins or so, and reading what it reported)
         sgp30.set_iaq_baseline(hex(int(config[CO2_BASELINE], 16)), hex(int(config[TVOC_BASELINE], 16)))
 
     return pm25, sgp30
 
 
 def run():
+    """
+    Main applicaiton loop. Initialize the sensors, initialize Honeycomb (if enabled), and begin taking readings. This
+    logic will read the sensors every N seconds, where N can be configured in the sample_frequency parameter of the
+    configuration file.
+    :return: None
+    """
     config = read_config()
     if config is None:
         print(f"No configuration found on: {CONFIG_PATH}")
+        exit(1)
 
+    # Setup the sensors and I²C bus
     pm25, sgp30 = init_electronics(config)
 
+    # Setup Honeycomb reporting...
     honeycomb_enabled = False
+
+    # NOTE: We use dict.get() here, not dict[key], since the keys may be missing!
     if config.get(WRITE_KEY) is not None and config.get(DATASET) is not None:
         libhoney.init(writekey=config[WRITE_KEY], dataset=config[DATASET])
         honeycomb_enabled = True
 
     since_baseline = 0
 
+    # Read some basic parameters from config, for our sensor loop...
     sample_freq = config.get(SAMPLE_FREQUENCY) or 60
     node_id = config.get(NODE_ID) or "unknown"
     quiet = config.get(QUIET_MODE) or False
 
+    # Loop forever...
     while True:
-        time.sleep(sample_freq)
-
         event = None
+        # If Honeycomb is available, initialize a new event for the sensors to use
         if honeycomb_enabled:
             event = libhoney.new_event()
             event.add_field("node", node_id)
 
+        # Read particulate data, then read CO₂ / VOC data...report it to screen and/or Honeycomb event
         read_particulates(pm25, event, quiet)
         since_baseline = read_volatiles(sgp30, since_baseline, event, quiet)
 
+        # If we're using Honeycomb, close/send the event.
         if honeycomb_enabled:
             event.send()
 
             # This is going to be a low-frequency sensor, so let's avoid buffering.
             libhoney.flush()
+
+        # Sleep for the number of seconds dictated by the sampling frequency, then we do another sensor pass
+        time.sleep(sample_freq)
 
 
 if __name__ == "__main__":
